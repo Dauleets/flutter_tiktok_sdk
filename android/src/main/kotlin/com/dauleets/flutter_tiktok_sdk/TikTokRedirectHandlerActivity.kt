3270967c225ef6e3c3e33ac1dc3a9a740c4ed964
Daulet Zhunussov <zhunussov.daulet.kz@gmail.com>
2025-01-10 13:57:03 +0500

    fix git і

--- android/src/main/kotlin/com/dauleets/flutter_tiktok_sdk/TikTokRedirectHandlerActivity.kt
+++ android/src/main/kotlin/com/dauleets/flutter_tiktok_sdk/TikTokRedirectHandlerActivity.kt
@@ -4,28 +4,40 @@ import android.app.Activity
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
-import android.util.Log
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.embedding.engine.FlutterEngineCache
+import io.flutter.plugin.common.MethodChannel
 
 class TikTokRedirectHandlerActivity : Activity() {
+
+    private lateinit var flutterEngine: FlutterEngine
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        // Инициализация FlutterEngine
+        flutterEngine = FlutterEngine(this)
+        flutterEngine.dartExecutor.executeDartEntrypoint(
+            io.flutter.embedding.engine.dart.DartExecutor.DartEntrypoint.createDefault()
+        )
+        FlutterEngineCache.getInstance().put("tiktok_engine", flutterEngine)
+
+        // Получение данных из Uri
         val uri = intent.data
         if (uri != null && uri.toString().startsWith("https://behype.io/tiktoksuccesslogin")) {
             val code = uri.getQueryParameter("code")
             val error = uri.getQueryParameter("error")
 
+            // Обработка данных
             if (!code.isNullOrEmpty()) {
-                MethodChannel(FlutterEngineCache.getInstance().get("tiktok_engine")!!.dartExecutor, "com.dauleets/flutter_tiktok_sdk")
+                MethodChannel(flutterEngine.dartExecutor.binaryMessenger, "com.dauleets/flutter_tiktok_sdk")
                     .invokeMethod("onCodeReceived", code)
             } else if (!error.isNullOrEmpty()) {
-                MethodChannel(FlutterEngineCache.getInstance().get("tiktok_engine")!!.dartExecutor, "com.dauleets/flutter_tiktok_sdk")
+                MethodChannel(flutterEngine.dartExecutor.binaryMessenger, "com.dauleets/flutter_tiktok_sdk")
                     .invokeMethod("onError", error)
             }
         }
 
-        finish()
+        finish() // Закрытие активности после обработки
     }
 }
